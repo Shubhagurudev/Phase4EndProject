bc6efd8c966aa5d281d3dffd4b09dd3c9b3b05c3
Shubhagurudev <shubhashubha99052@gmail.com>
2023-12-15 10:03:14 +0530

    test commit

---
+++ DevelopingForTest/code.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopingForTest
+{
+    public enum Pizza
+    {
+        Veggie,
+        Cheese,
+        Pepporoni,
+        Hawaiian,
+        BBQchicken,
+        Maegherita
+
+    }
+    public class code
+    {
+        public Pizza PizzaType { get; set; }
+    }
+
+    public class OrderDetails
+    {
+        public int OrderId { get; set; }
+        public string PizzaName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+    }
+
+    public class PriceCalculation
+    {
+        public void Calculate(OrderDetails orderedpizza, code pizza)
+        {
+            if (pizza.PizzaType == Pizza.Veggie)
+            {
+                orderedpizza.Price = 190;
+
+            }
+            else if (pizza.PizzaType == Pizza.Maegherita)
+            {
+                orderedpizza.Price = 250;
+            }
+            else if (pizza.PizzaType == Pizza.Pepporoni)
+            {
+                orderedpizza.Price = 300;
+            }
+            else if (pizza.PizzaType == Pizza.BBQchicken)
+            {
+                orderedpizza.Price = 500;
+            }
+            else if (pizza.PizzaType == Pizza.Hawaiian)
+            {
+                orderedpizza.Price = 400;
+            }
+            else if (pizza.PizzaType == Pizza.Cheese)
+            {
+                orderedpizza.Price = 450;
+            }
+
+        }
+    }
+}
---
+++ Testing/test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevelopingForTest;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace Testing
+{
+    [TestFixture]
+    public class test
+    {
+        [TestCase]
+
+        public void testing()
+        {
+            code co =  new code { PizzaType = Pizza.Veggie };
+
+            OrderDetails od = new OrderDetails { OrderId = 107, PizzaName = "Veggie", Quantity = 1, Price = 390 };
+
+            PriceCalculation c = new PriceCalculation();
+
+            c.Calculate(od,co);
+
+            ClassicAssert.AreEqual(od.Price, 190);
+
+        }
+    }
+}
